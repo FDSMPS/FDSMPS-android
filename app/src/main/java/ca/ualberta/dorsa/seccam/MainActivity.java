58e2a650440258361327a7dfc0199d881357b9d2
Dorsa Nahid <31228267+dorsaNahid@users.noreply.github.com>
2020-01-09 17:33:43 -0700

    Intial commit

---
+++ app/src/main/java/ca/ualberta/dorsa/seccam/MainActivity.java
@@ -0,0 +1,13 @@
+package ca.ualberta.dorsa.seccam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
