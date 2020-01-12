f03bee902e7bd2ea9a3db870c47d090f79ac99e1
Dorsa Nahid <31228267+dorsaNahid@users.noreply.github.com>
2020-01-12 12:58:50 -0700

    QR code scanner implemented

--- app/src/main/java/ca/ualberta/dorsa/seccam/MainActivity.java
+++ app/src/main/java/ca/ualberta/dorsa/seccam/MainActivity.java
@@ -1,15 +1,45 @@
 package ca.ualberta.dorsa.seccam;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 import androidx.appcompat.app.AppCompatActivity;
 import ca.ualberta.dorsa.myapplication.R;
 
 public class MainActivity extends AppCompatActivity {
+    String qrCodeScanned = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    public void openCamera(View view) {
+        new IntentIntegrator(this).initiateScan(); // `this` is the current Activity
+
+    }
+    // Get the results:
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+                qrCodeScanned = result.getContents();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+
 }
