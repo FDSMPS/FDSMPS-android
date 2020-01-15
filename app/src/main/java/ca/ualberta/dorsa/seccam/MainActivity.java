319b96519178702880532b4a6936f17eb8335aa1
Dorsa Nahid <31228267+dorsaNahid@users.noreply.github.com>
2020-01-15 12:06:23 -0700

    Revert "non functioning sign up"
    This reverts commit bab9f95b381ea23f973a36797a076fa4f9578583.

--- app/src/main/java/ca/ualberta/dorsa/seccam/MainActivity.java
+++ app/src/main/java/ca/ualberta/dorsa/seccam/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
 
     }
 
@@ -42,6 +42,4 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void onLoginClick(View view) {
-    }
 }
