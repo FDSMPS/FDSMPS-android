c9a763ad8897960d6de2399ad0d71dc9e8bfcd6b
Jessica D'Cunha <35900289+jrdcunha@users.noreply.github.com>
2020-01-15 13:46:37 -0700

    initial

---
+++ app/src/main/java/ca/ualberta/dorsa/seccam/LogActivity.java
@@ -0,0 +1,30 @@
+package ca.ualberta.dorsa.seccam;
+
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import ca.ualberta.dorsa.myapplication.R;
+
+public class LogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+    }
+
+}
