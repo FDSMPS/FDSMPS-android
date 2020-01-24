098fd02c46f8c32bdcc64717443cec1f09184618
Dorsa Nahid <31228267+dorsaNahid@users.noreply.github.com>
2020-01-23 17:54:12 -0700

    login and sign up

---
+++ app/src/main/java/ca/ualberta/dorsa/seccam/loginActivity.java
@@ -0,0 +1,26 @@
+package ca.ualberta.dorsa.seccam;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import ca.ualberta.dorsa.myapplication.R;
+
+
+public class loginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void login(View view) {
+
+
+    }
+
+    public void signUp(View view) {
+
+    }
+}
---
+++ app/src/main/java/ca/ualberta/dorsa/seccam/signUp.java
@@ -0,0 +1,15 @@
+package ca.ualberta.dorsa.seccam;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import ca.ualberta.dorsa.myapplication.R;
+
+public class signUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
