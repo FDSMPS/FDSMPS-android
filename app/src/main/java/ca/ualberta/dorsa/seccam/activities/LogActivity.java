edbb29f4d60b87134730afd0fb2a7f623986afd9
Dorsa Nahid <31228267+dorsaNahid@users.noreply.github.com>
2020-01-27 20:35:46 -0700

    new update to database

--- app/src/main/java/ca/ualberta/dorsa/seccam/activities/LogActivity.java
+++ app/src/main/java/ca/ualberta/dorsa/seccam/activities/LogActivity.java
@@ -72,6 +72,15 @@ public class LogActivity extends AppCompatActivity {
                 FirebaseDatabase.getInstance().getReference("Users/" + FirebaseAuth.getInstance().getCurrentUser().getUid())
                         .child("cameraCode")
                         .setValue(qrCodeScanned);
+
+                FirebaseDatabase.getInstance().getReference("Users/" + FirebaseAuth.getInstance().getCurrentUser().getUid())
+                        .child("userSetting")
+                        .child("cameraCode")
+                        .setValue(qrCodeScanned);
+
+                FirebaseDatabase.getInstance().getReference("SecurityCameras/" + qrCodeScanned).child("registered").setValue("true");
+                FirebaseDatabase.getInstance().getReference("SecurityCameras/" + qrCodeScanned).child("registeredUserName").setValue(FirebaseAuth.getInstance().getCurrentUser().getUid());
+                FirebaseDatabase.getInstance().getReference("SecurityCameras/" + qrCodeScanned).child("cameraCode").setValue(qrCodeScanned);
             }
         } else {
             super.onActivityResult(requestCode, resultCode, data);
