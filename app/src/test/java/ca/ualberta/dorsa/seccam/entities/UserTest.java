cf3dd23bdf4913b04968bb9f93c734f411902428
Jessica D'Cunha <35900289+jrdcunha@users.noreply.github.com>
2020-02-17 15:19:31 -0700

    created UserTest file

---
+++ app/src/test/java/ca/ualberta/dorsa/seccam/entities/UserTest.java
@@ -0,0 +1,18 @@
+package ca.ualberta.dorsa.seccam.entities;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * To test the methods in the User file
+ */
+public class UserTest {
+    private User u;
+
+    @Before
+    public void setUp() {
+    }
+}
